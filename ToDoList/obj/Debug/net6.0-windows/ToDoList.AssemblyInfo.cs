25a38278ecc59a22df6f1261ff8fdccb8bb3863a
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-05-18 23:54:29 +0100

    commit all

--- ToDoList/obj/Debug/net6.0-windows/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net6.0-windows/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c9c180fdce5e27fae3803ed6dc371bf7290a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
